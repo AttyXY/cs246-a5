df5244c7e2180751361ff4987180f114720d015f
Atticus Yang <atticus.yang@uwaterloo.ca>
2022-10-15 12:14:23 -0400

    update commit

---
+++ runTests.sh
@@ -0,0 +1,7 @@
+cp chess tests/pieces/basicMoves
+cp chess tests/check
+cp chess tests/checkmate
+cp chess tests/stalemate
+cp chess tests/castling
+cp chess tests/enPassant
+cp chess tests/pawnPromotion
